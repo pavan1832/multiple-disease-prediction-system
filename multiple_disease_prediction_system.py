96d1388beaddf81536efb079bfd3a7f5d9debfb1
pavan1832 <141185596+pavan1832@users.noreply.github.com>
2024-06-02 19:53:58 +0530

    Update multiple_disease_prediction_system.py

--- multiple_disease_prediction_system.py
+++ multiple_disease_prediction_system.py
@@ -6,8 +6,10 @@ from streamlit_option_menu import option_menu
 def show_main_page():
     st.subheader('We Care Hospitals')
     st.title('Successfully Logged in')
-
-
+    
+diabetes_model = pickle.load(open("diabetes_model.sav", "rb"))
+heart_disease_model = pickle.load(open("heart_disease_model.sav", "rb"))
+parkinsons_model = pickle.load(open("parkinsons_model.sav", "rb"))
 
 # Display main page header
 show_main_page()
